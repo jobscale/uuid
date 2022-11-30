070b2b11fc03f14fd253dfc3cec3c60869dc29aa
jobscale <jobscale@users.noreply.github.com>
2022-12-01 03:02:38 +0900

    eslintrc updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2, { MemberExpression: 0 }],
+    'no-trailing-spaces': 'error',
     'arrow-parens': 'off',
     'class-methods-use-this': 'off',
     'no-await-in-loop': 'off',
