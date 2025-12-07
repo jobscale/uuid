375d0ef8e26362521a1ffb73dcf4d2f8dd6d29f9
jobscale <jobscale@users.noreply.github.com>
2025-12-08 01:15:36 +0900

    eslint configs node,jest,browser added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+import crypto from 'crypto';
+
 export class UUID {
   constructor() {
     this.uuid = this.v4;
   }
 
   v4() {
+    return crypto.randomUUID();
+  }
+
+  myself() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
       const r = Math.random() * 16 | 0;
-      const v = c === 'x' ? r : ((r & 0x3) | 0x8);
+      const v = c === 'x' ? r : r & 0x3 | 0x8;
       return v.toString(16);
     });
   }
